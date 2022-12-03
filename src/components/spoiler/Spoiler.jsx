e4d0c28db9018593fd1832032d2a1eeae81fde14
Сергей Субботин <zarodiny@yandex.ru>
2022-12-03 20:12:54 +1000

    добавляет удаление из БД

--- src/components/spoiler/Spoiler.jsx
+++ src/components/spoiler/Spoiler.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 
 import { storage } from '../../firebaseConfig';
-import { ref, uploadBytes, listAll, getDownloadURL } from 'firebase/storage';
+import { ref, uploadBytes, listAll, getDownloadURL, deleteObject } from 'firebase/storage';
 
 import { SpoilerWrapper } from "./styled"
 
@@ -11,21 +11,35 @@ export default function Spoiler({ isOpenSpoiler, id }) {
     const [imageList, setImageList] = useState([]);
 
     // получает ссылку на список файлов из БД
-    const imageRefList = ref(storage, "images/");
-
+    const imageRefList = ref(storage, `images/${id}`);
 
     const uploadImages = () => {
         if (imageUpload == null) return;
 
-        //генерирует новый путь и имя к файлу
-        const imageRef = ref(storage, `images/${imageUpload.name + id}`);
+        //генерирует новый путь и имя файла
+        const imageRef = ref(storage, `images/${id}/${imageUpload.name + id}`);
 
         //загружает файл в БД из состояния "imageUpload" по пути "imageRef"
         uploadBytes(imageRef, imageUpload).then(() => {
             alert('изображение загружено');
+        }).catch((error) => {
+            console.log("не удалось загрузить изображение", error);
         });
     };
 
+    const deleteImages = () => {
+        // получает ссылку на задачу (в которую вложены все изображения)
+        const listRef = ref(storage, `images/${id}`);
+        listAll(listRef)
+            .then((res) => {
+                res.items.forEach((item) => {
+                    deleteObject(item);
+                });
+                alert('Изображения удалены');
+            }).catch((error) => {
+                console.log("не удалось удалить", error);
+            });
+    }
 
     useEffect(() => {
         listAll(imageRefList).then((response) => {
@@ -37,6 +51,7 @@ export default function Spoiler({ isOpenSpoiler, id }) {
         });
     }, [])
 
+    console.log(imageList);
 
     return (
         <SpoilerWrapper isOpenSpoiler={isOpenSpoiler} >
@@ -48,6 +63,10 @@ export default function Spoiler({ isOpenSpoiler, id }) {
             <button
                 onClick={uploadImages}
             >загрузить файл</button>
+
+            <button
+                onClick={deleteImages}
+            >удалить файлы</button>
 
             {
                 imageList.map((url) => {
